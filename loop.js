1b218da7049caf99081b5af2f9c513d139aaffd8
Semira kemal <Ksemira225@gmail.com>
2025-07-15 23:52:41 +0300

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,27 @@
+//question 1:count first 10 number
+for(let j = 1; j < 11; j++){
+	console.log(j)
+	}
+//question 2: count next 5 number
+function printNextFive(num){
+	for(let i = 1; i <= 5; i++){
+		console. log(num + i)
+		}
+	}
+	printNextFive(8);
+//question 3:sum of next five number
+function printNextFive(num){
+	let sum = 0
+	for(let k = 1; k <= 5; k++){
+		if(num + k){
+		sum += num + k;
+		}
+		console. log(sum)
+		}
+	 }
+		printNextFive(8);
+//question 4:print all arrays
+let a = [1,"hellow",8,44]
+for(i = 0; i < a. length;i++){
+console.log(a[i])
+}
