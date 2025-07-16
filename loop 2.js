b884e3a4083fbd18d958e306a46487a2893afc80
Semira kemal <Ksemira225@gmail.com>
2025-07-16 23:52:12 +0300

    Add files via upload

---
+++ loop 2.js
@@ -0,0 +1,15 @@
+//question:5 total number of element found in arrays
+let a = [1,"hellow",8,44];
+let b = ["world", 13];
+console.log(a.length);
+console. log(b.length);
+// question:6 sum of the arrays
+let c = [5,6,99,8,76,4,68,44];
+let c = [3,0]
+let sum = 0;
+for(let i = 0;i<c.length;i++){
+	sum += c[i];
+}
+console.log(sum);
+
+
