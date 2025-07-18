fecb22eccbb486c0b93d2e1092847a7bd78f6c77
Semira kemal <Ksemira225@gmail.com>
2025-07-18 22:07:12 +0300

    Update Day 3.js

--- Day 3.js
+++ Day 3.js
@@ -1,15 +1,30 @@
-//question:5 total number of element found in arrays
-let a = [1,"hellow",8,44];
-let b = ["world", 13];
-console.log(a.length);
-console. log(b.length);
-// question:6 sum of the arrays
-let c = [5,6,99,8,76,4,68,44];
-let c = [3,0]
-let sum = 0;
+//question: 7 calculate all total sum of  Even,odd and substrate them
+/let c = [5,6,99,8,76,4,68,44];
+let odd = 0;
+let even =0;
+let difference = 0;
 for(let i = 0;i<c.length;i++){
-	sum += c[i];
+	if(c[i]%2==0){
+		even += c[i];
+		}
+	else{
+      odd += c[i];
+      }
+
 }
-console.log(sum);
+difference = even - odd;	
+console.log(difference);  
+//question:8
+let b = [5,6,99,8,76,4,68,44];
+let evenIndex = 0;
+function printevenIndex(b){
+   for(let i =0;i <b.length; i++){
+  	if(i%2==0){
+	 	console.log(b[i]);
+  	}
+    }
+}
+printevenIndex(b);
+
 
 
