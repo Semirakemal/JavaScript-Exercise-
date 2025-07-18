85bb89bafa8a51cbc5256083c137b377bf306f3b
Semira kemal <Ksemira225@gmail.com>
2025-07-18 23:48:44 +0300

    Add files via upload

---
+++ Day 4.js
@@ -0,0 +1,10 @@
+// define function and call function
+function myFirst() {	
+    console. log("hellow" );
+}
+myFirst();
+// print parameter 
+function mySecond(a){
+	console. log(a)
+}
+mySecond("cat" )
